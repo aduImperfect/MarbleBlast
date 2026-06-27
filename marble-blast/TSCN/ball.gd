549c4e76a1de47faea451bc5e215fd273b87a8a3
Aditya Subramanian <adu101992@gmail.com>
2026-06-27 20:31:16 +0530

    Shifted the impulse based movement to use forward and right vectors instead of the forced z axis movement.

--- marble-blast/TSCN/ball.gd
+++ marble-blast/TSCN/ball.gd
@@ -1,6 +1,8 @@
 extends RigidBody3D
 
-
+@export var forward_vector : Vector3 = -global_transform.basis.z
+@export var right_vector   : Vector3 = global_transform.basis.x
+@export var up_vector      : Vector3 = global_transform.basis.y
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,13 +20,16 @@ func _physics_process(_delta: float) -> void:
 func _input(_event: InputEvent) -> void:
 	if _event is InputEventKey and _event.keycode == KEY_UP:
 		if _event.is_pressed():
-			#print("KEY UP BEING PRESSED!")
-			#apply_force(Vector3(0.0,0.0,-1.0))
-			apply_impulse(Vector3(0.0,0.0,-1.0))
+			apply_impulse(forward_vector)
 
 	if _event is InputEventKey and _event.keycode == KEY_DOWN:
 		if _event.is_pressed():
-			#print("KEY DOWN BEING PRESSED!")
-			#apply_force(Vector3(0.0,0.0,-1.0))
-			apply_impulse(Vector3(0.0,0.0,1.0))
-			pass
+			apply_impulse(-forward_vector)
+
+	if _event is InputEventKey and _event.keycode == KEY_LEFT:
+		if _event.is_pressed():
+			apply_impulse(-right_vector)
+
+	if _event is InputEventKey and _event.keycode == KEY_RIGHT:
+		if _event.is_pressed():
+			apply_impulse(right_vector)
